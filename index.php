d9e62cc1850897bba32f2084e855f120e1edd417
unknown <siavashizadkhasti@yahoo.com>
2014-06-01 12:25:08 +0430

    checks

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>       
+        <div>
+            <?php            
+                $allCountries = new countries;
+                $count->getCountryInformation();
+            ?>
+        </div>
+    </body>
+</html>
---
+++ vendor/bin/phpcpd.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+SET BIN_TARGET=%~dp0/../sebastian/phpcpd/phpcpd
+php "%BIN_TARGET%" %*
