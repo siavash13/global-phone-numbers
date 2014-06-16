0bc8ee40ccda859af6c7c271a23d9c7b710e0529
unknown <siavashizadkhasti@yahoo.com>
2014-06-16 13:10:19 +0430

    Unix ending lines

--- puppet/manifests/phpbase.pp
+++ puppet/manifests/phpbase.pp
@@ -14,12 +14,93 @@ exec
         require => Exec['add php55 apt-repo']
 }
 
-include other #curl and sqlite
+exec
+{
+    'apt-get install dos2unix':
+        command => '/usr/bin/apt-get install dos2unix'
+}
+
+exec
+{
+    'dos2unix checks.sh':
+        command => 'dos2unix /var/www/public/global-phone-numbers/tests.sh',
+        require => Exec['apt-get install dos2unix']
+}
+
+exec
+{
+    'dos2unix unitTest.sh':
+        command => 'dos2unix /var/www/public/global-phone-numbers/unitTest.sh',
+        require => Exec['apt-get install dos2unix']
+}
+
+exec
+{
+    'dos2unix callRpc.php':
+        command => 'dos2unix /var/www/public/global-phone-numbers/Main.php',
+        require => Exec['apt-get install dos2unix']
+}
+
+exec
+{
+    'dos2unix dbConfig.php':
+        command => 'dos2unix /var/www/public/global-phone-numbers/MainRPC.php',
+        require => Exec['apt-get install dos2unix']
+}
+
+exec
+{
+    'dos2unix DbModel.php':
+        command => 'dos2unix /var/www/public/global-phone-numbers/Client.php',
+        require => Exec['apt-get install dos2unix']
+}
+
+exec
+{
+    'dos2unix NumberLookup.php':
+        command => 'dos2unix /var/www/public/global-phone-numbers/index.php',
+        require => Exec['apt-get install dos2unix']
+}
+
+exec
+{
+    'dos2unix rpcIndex.php':
+        command => 'dos2unix /var/www/public/global-phone-numbers/compute.php',
+        require => Exec['apt-get install dos2unix']
+}
+
+
+exec
+{
+    'dos2unix rpcIndex.php':
+        command => 'dos2unix /var/www/public/global-phone-numbers/UnitTest.php',
+        require => Exec['apt-get install dos2unix']
+}
+
+
+exec
+{
+    'dos2unix rpcIndex.php':
+        command => 'dos2unix /var/www/public/global-phone-numbers/lib/Config.php',
+        require => Exec['apt-get install dos2unix']
+}
+
+
+exec
+{
+    'dos2unix rpcIndex.php':
+        command => 'dos2unix /var/www/public/global-phone-numbers/lib/DBConfig2.php',
+        require => Exec['apt-get install dos2unix']
+}
+
+include bootstrap
+include other #curl
 include php55 #specific setup steps for 5.5
 include php
 include apache
 include mysql
 include phpmyadmin
-
+include beanstalkd
+include redis
+include memcached
 include composer
-
